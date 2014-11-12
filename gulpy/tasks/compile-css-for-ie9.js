fb62bb739a1abe63461de3dcc964898f4169d792
Artem Malko <a.malko@2gis.ru>
2014-11-13 00:56:45 +0600

    initial commit for tars-less

---
+++ gulpy/tasks/compile-css-for-ie9.js
@@ -0,0 +1,78 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var less = require('gulp-less');
+var gulpif = require('gulp-if');
+var gutil = require('gulp-util');
+var plumber = require('gulp-plumber');
+var autoprefix = require('gulp-autoprefixer');
+var notify = require('gulp-notify');
+var projectConfig = require('../../projectConfig');
+var notifyConfig = projectConfig.notifyConfig;
+var modifyDate = require('../helpers/modifyDateFormatter');
+var browserSync = require('browser-sync');
+
+var lessFilesToConcatinate = [
+        './markup/' + projectConfig.fs.staticFolderName + '/less/normalize.less',
+        './markup/' + projectConfig.fs.staticFolderName + '/less/mixins.less',
+        './markup/' + projectConfig.fs.staticFolderName + '/less/spritesLess/sprite96.less',
+        './markup/' + projectConfig.fs.staticFolderName + '/less/spritesLess/sprite.less'
+    ];
+
+    if (projectConfig.useSVG) {
+        lessFilesToConcatinate.push(
+            './markup/' + projectConfig.fs.staticFolderName + '/less/spritesLess/svg-fallback-sprite.less',
+            './markup/' + projectConfig.fs.staticFolderName + '/less/spritesLess/svg-sprite.less'
+        );
+    }
+
+    lessFilesToConcatinate.push(
+        './markup/' + projectConfig.fs.staticFolderName + '/less/fonts.less',
+        './markup/' + projectConfig.fs.staticFolderName + '/less/vars.less',
+        './markup/' + projectConfig.fs.staticFolderName + '/less/GUI.less',
+        './markup/' + projectConfig.fs.staticFolderName + '/less/common.less',
+        './markup/' + projectConfig.fs.staticFolderName + '/less/plugins/**/*.less',
+        './markup/modules/*/*.less',
+        './markup/modules/*/ie/ie9.less',
+        './markup/' + projectConfig.fs.staticFolderName + '/less/etc/**/*.less'
+    );
+
+/**
+ * Less compilation for ie9
+ * @param  {object} buildOptions
+ */
+module.exports = function(buildOptions) {
+
+    return gulp.task('compile-css-for-ie9', function(cb) {
+        if (gutil.env.ie9) {
+            return gulp.src(lessFilesToConcatinate)
+                .pipe(plumber())
+                .pipe(concat('main_ie9' + buildOptions.hash + '.css'))
+                .pipe(less())
+                .on('error', notify.onError(function (error) {
+                    return '\nAn error occurred while compiling css for ie9.\nLook in the console for details.\n' + error;
+                }))
+                .pipe(autoprefix('ie 9', { cascade: true }))
+                .on('error', notify.onError(function (error) {
+                    return '\nAn error occurred while autoprefixing css.\nLook in the console for details.\n' + error;
+                }))
+                .pipe(gulp.dest('./dev/' + projectConfig.fs.staticFolderName + '/css/'))
+                .pipe(browserSync.reload({stream:true}))
+                .pipe(
+                    gulpif(notifyConfig.useNotify,
+                        notify({
+                            onLast: true,
+                            sound: notifyConfig.sounds.onSuccess,
+                            title: notifyConfig.title,
+                            message: 'Less-files for ie9 have been compiled. \n'+ notifyConfig.taskFinishedText +'<%= options.date %>',
+                            templateOptions: {
+                                date: modifyDate.getTimeOfModify()
+                            }
+                        })
+                    )
+                );
+        } else {
+            gutil.log('!Stylies for ie9 are not used!');
+            cb(null);
+        }
+    });
+};
---
+++ gulpy/tasks/compile-css.js
@@ -0,0 +1,74 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var less = require('gulp-less');
+var gulpif = require('gulp-if');
+var gutil = require('gulp-util');
+var autoprefix = require('gulp-autoprefixer');
+var notify = require('gulp-notify');
+var projectConfig = require('../../projectConfig');
+var notifyConfig = projectConfig.notifyConfig;
+var modifyDate = require('../helpers/modifyDateFormatter');
+var browserSync = require('browser-sync');
+
+var lessFilesToConcatinate = [
+        './markup/' + projectConfig.fs.staticFolderName + '/less/normalize.less',
+        './markup/' + projectConfig.fs.staticFolderName + '/less/mixins.less',
+        './markup/' + projectConfig.fs.staticFolderName + '/less/spritesLess/sprite96.less',
+        './markup/' + projectConfig.fs.staticFolderName + '/less/spritesLess/sprite.less'
+    ],
+
+    autoprefixerConfig = projectConfig.autoprefixerConfig.join(',');
+
+    if (projectConfig.useSVG) {
+        lessFilesToConcatinate.push(
+            './markup/' + projectConfig.fs.staticFolderName + '/less/spritesLess/svg-fallback-sprite.less',
+            './markup/' + projectConfig.fs.staticFolderName + '/less/spritesLess/svg-sprite.less'
+        );
+    }
+
+    lessFilesToConcatinate.push(
+        './markup/' + projectConfig.fs.staticFolderName + '/less/fonts.less',
+        './markup/' + projectConfig.fs.staticFolderName + '/less/vars.less',
+        './markup/' + projectConfig.fs.staticFolderName + '/less/GUI.less',
+        './markup/' + projectConfig.fs.staticFolderName + '/less/common.less',
+        './markup/' + projectConfig.fs.staticFolderName + '/less/plugins/**/*.less',
+        './markup/modules/*/*.less',
+        './markup/' + projectConfig.fs.staticFolderName + '/less/etc/**/*.less'
+    );
+
+/**
+ * Less compilation
+ * @param  {object} buildOptions
+ */
+module.exports = function(buildOptions) {
+
+    return gulp.task('compile-css', function(cb) {
+        gulp.src(lessFilesToConcatinate)
+            .pipe(concat('main' + buildOptions.hash + '.css'))
+            .pipe(less())
+            .on('error', notify.onError(function (error) {
+                return '\nAn error occurred while compiling css.\nLook in the console for details.\n' + error;
+            }))
+            .pipe(autoprefix(autoprefixerConfig, { cascade: true }))
+            .on('error', notify.onError(function (error) {
+                return '\nAn error occurred while autoprefixing css.\nLook in the console for details.\n' + error;
+            }))
+            .pipe(gulp.dest('./dev/' + projectConfig.fs.staticFolderName + '/css/'))
+            .pipe(browserSync.reload({stream:true}))
+            .pipe(
+                gulpif(notifyConfig.useNotify,
+                    notify({
+                        onLast: true,
+                        sound: notifyConfig.sounds.onSuccess,
+                        title: notifyConfig.title,
+                        message: 'Less-files\'ve been compiled. \n'+ notifyConfig.taskFinishedText +'<%= options.date %>',
+                        templateOptions: {
+                            date: modifyDate.getTimeOfModify()
+                        }
+                    })
+                )
+            );
+
+            cb(null);
+        });
+};
