b50292737f51a43d1cf228926c55d613237a145c
Artem Malko <artem.malko@gmail.com>
2015-09-06 19:19:27 +0600

    fix padding between images

--- tars/tasks/make-sprite.js
+++ tars/tasks/make-sprite.js
@@ -46,6 +46,7 @@ module.exports = function () {
                                 dpi288: dpi288,
                                 dpi384: dpi384
                             },
+                            padding: (i + 1) * 4,
                             cssTemplate: './markup/' + staticFolderName + '/less/sprite-generator-templates/less.sprite.mustache'
                         }
                     )
